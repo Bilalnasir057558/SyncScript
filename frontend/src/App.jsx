09c6643e3cb3e482f2d889705eeb884fa5ca1a4a
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-10 19:50:08 +0500

    Comment the testing of input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,18 @@ import './App.css'
 import Input from './components/Input'
 
 function App() {
-  const [name, setName] = useState("")
+  // const [name, setName] = useState("")
 
-  return (
-    <div>
-      <Input 
-        type="text"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        placeholder="E.g. Dr. Julian Vane"
-      />
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <Input 
+  //       type="text"
+  //       value={name}
+  //       onChange={e => setName(e.target.value)}
+  //       placeholder="E.g. Dr. Julian Vane"
+  //     />
+  //   </div>
+  // )
 }
 
 export default App

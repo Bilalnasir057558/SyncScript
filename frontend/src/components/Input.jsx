9ee717bdd24794cde513f5eb9d8ab3e95051ff27
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-10 19:39:45 +0500

    Configure tailwindcss

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,7 @@
 export default function Input({
     type, 
     value, 
+    onChange,
     placeholder,
     className = "rounded-md px-4 py-2 focus:ring-2 focus:ring-blue-400 placeholder:text-gray-500",
     ...props
@@ -9,6 +10,7 @@ export default function Input({
         <input
             type={type}
             value={value}
+            onChange={onChange}
             placeholder={placeholder}
             className={className}
             {...props}

b1507efdc14a16b7b1658e3600a3190c0528badc
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-10 19:45:51 +0500

    Fix outlining

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -3,7 +3,7 @@ export default function Input({
     value, 
     onChange,
     placeholder,
-    className = "rounded-md px-4 py-2 focus:ring-2 focus:ring-blue-400 placeholder:text-gray-500",
+    className = "rounded-lg px-4 py-2 text-sm bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-300 placeholder:text-gray-500",
     ...props
 }) {
     return (

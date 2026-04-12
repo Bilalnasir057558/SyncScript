68ebab5459b513523f01a38386a8155b97c8b4ab
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-12 11:29:12 +0500

    Add Icon component

---
+++ frontend/src/components/Icon.jsx
@@ -0,0 +1,16 @@
+export default function Icon({
+    name,
+    size,
+    color = "CurrentColor",
+    ...props
+}) {
+    return <svg
+        width={size}
+        height={size}
+        fill={color}
+        {...props}
+        aria-hidden="true"
+    >
+        <use href={`../../sprite.svg#icon-${name}`} />
+    </svg>
+}

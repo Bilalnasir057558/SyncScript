0541cfbdc9f08544e1b104d084c98205dd11d014
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-12 20:23:45 +0500

    Add three-column layout for methodology section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import FeaturesSection from './components/FeaturesSection'
 import HeroSection from './components/HeroSection'
+import MethodologySection from './components/MethodologySection'
 import Login from './pages/Login'
 
 function App() {
   return (
     // <HeroSection />
-    <FeaturesSection />
+    // <FeaturesSection />
+    <MethodologySection />
   )
 }
 

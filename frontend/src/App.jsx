a08c41abd271952492b3494f26e62dc8e7683088
Muhammad Adil Shaikh <as3160049@gmail.com>
2026-04-17 00:53:56 +0500

    Resource detail page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import LandingPage from './pages/LandingPage';
 import DashboardPage from './pages/DashboardPage';
 import AddResourceForm from './components/AddResourceForm';
+import ResourceDetail from './pages/ResourceDetail';
 import CreateVaultModal from './components/CreateVault';
 
 function App() {
   return (
     // <DashboardPage />
-    <AddResourceForm />
+    // <AddResourceForm />
+    <ResourceDetail/>
   )
 }
 

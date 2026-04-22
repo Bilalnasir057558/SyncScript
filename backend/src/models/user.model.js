438b3b6f5494cde02741fcab57047f018b57351c
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-22 20:33:34 +0500

    Add user model with password encryption and validation

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true});
+
+// before saving db object, encrypt the password
+userSchema.pre('save', async function() {
+    if(!this.isModified('password')) return; // returns if password is not changed
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+// Schema methods => attached to each document
+userSchema.methods.isPasswordCorrect = async (password) => {
+    return await bcrypt.compare(password, this.password);
+}
+
+export const User = mongoose.model('User', userSchema);

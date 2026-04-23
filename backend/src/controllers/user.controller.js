afd3b98e2a13621b3fcf948aaba05b2e426efd50
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-23 11:10:02 +0500

    Implement logout functionality and enhance JWT verification middleware

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -127,7 +127,34 @@ const loginUser = asyncHandler(async (req, res) => {
     )
 })
 
+const logoutUser = asyncHandler(async (req, res) => {
+    
+    // logged in user can logout -> access to req.user
+    await User.findByIdAndUpdate(
+        user.req?.user._id,
+        {
+           $set: { refreshToken: undefined }
+        },
+        {
+            returnDocument: "after"
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
+    .json(
+        new ApiResponse(200, {}, 'User logged out successfully.')
+    )
+})
+
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 
 
-const verifyJWT = async (req, res) => {
+const verifyJWT = async (req, res, next) => {
     try {
         
         if(!req.cookies.accessToken) {
@@ -21,11 +21,11 @@ const verifyJWT = async (req, res) => {
         const decoded = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 
         if(!decoded) {
-            throw new ApiError(400, 'Access token is expired or used.');
+            throw new ApiError(400, 'Access token is invalid.');
         }
 
         // if access token is valid => get user details from it
-        const user = await User.findById(decoded._id).select("-password -refreshToken");
+        const user = await User.findById(decoded._id).select("-password");
 
         if(!user) {
             throw new ApiError(400, 'Access token is expired or used.');
@@ -33,9 +33,10 @@ const verifyJWT = async (req, res) => {
 
         // attach user to req
         req.user = user;
+        next();
 
     } catch (error) {
-        throw new ApiError(400, 'Unauthorized request.');
+        throw new ApiError(400, error?.message || 'Invalid access token.');
     }
 
 }

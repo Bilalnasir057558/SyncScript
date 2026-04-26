98347bd625fef388c2728b0e4ef2ed3498a827d5
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-04-26 13:11:46 +0500

    Add vault endpoints for user vault management

--- backend/src/routes/vault.routes.js
+++ backend/src/routes/vault.routes.js
@@ -1,12 +1,32 @@
 import Router from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
-import { createVault } from "../controllers/vault.controller.js";
+import { createVault, deleteVault, getUserVaults, getVaultById, updateVault } from "../controllers/vault.controller.js";
 
 const router = Router();
 
 router.route('').post(
     verifyJWT,
     createVault
+)
+
+router.route('').get(
+    verifyJWT,
+    getUserVaults
+)
+
+router.route('/:vaultId').get(
+    verifyJWT,
+    getVaultById
+)
+
+router.route('/:vaultId').put(
+    verifyJWT,
+    updateVault
+)
+
+router.route('/:vaultId').delete(
+    verifyJWT,
+    deleteVault
 )
 
 export default router;
